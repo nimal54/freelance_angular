33e73fcb68815138ceb405121330731d8722144f
nimal54 <nimalviju54@gmail.com>
2021-01-03 03:40:55 +0000

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
 
+import {MatSelectModule} from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { DemoPageComponent } from './pages/demo-page/demo-page.component';
 import { UtilityCardComponent } from './components/Cards/utility-card/utility-card.component';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,9 +32,19 @@ import { UtilityCardComponent } from './components/Cards/utility-card/utility-ca
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
-  providers: [],
+  exports:[
+    MatSelectModule
+  ],
+  providers: [
+    {provide:MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearence: 'fill'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/Cards/utility-card/utility-card.component.ts
+++ src/app/components/Cards/utility-card/utility-card.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+interface stateSelector {
+  value: string;
+  viewValue: string;
+}
+interface operatorSelector {
+  value: string;
+  viewValue: string;
+}
+interface cusIdSelector {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-utility-card',
   templateUrl: './utility-card.component.html',
   styleUrls: ['./utility-card.component.scss']
 })
-export class UtilityCardComponent implements OnInit {
+export class UtilityCardComponent {
 
-  constructor() { }
+  form: FormGroup;
+  stateSelectorList: stateSelector[] = [
+    { value: '0', viewValue: 'Karnadaka' },
+    { value: '1', viewValue: 'Kerala' },
+    { value: '2', viewValue: 'Maharashra' }
+  ];
+  operatorSelectorList: operatorSelector[] = [
+    { value: '3', viewValue: 'Operator1' },
+    { value: '4', viewValue: 'Operator2' },
+    { value: '5', viewValue: 'Operator3' }
+  ];
+  cusIdSelectorList: cusIdSelector[] = [
+    { value: '3', viewValue: 'Cus!' },
+    { value: '4', viewValue: 'Cus2' },
+    { value: '5', viewValue: 'Cus3' }
+  ];
+
+
+  stateSelectorController = new FormControl(this.stateSelectorList[2].value);
+  operatorSelectorController = new FormControl(this.operatorSelectorList[2].value);
+  cusIdSelectorController = new FormControl(this.operatorSelectorList[1].value);
+
+
+  constructor() {
+    this.form = new FormGroup({
+      state: this.stateSelectorController,
+      operator: this.operatorSelectorController,
+      cusId: this.cusIdSelectorController
+    });
+  }
 
   ngOnInit(): void {
   }
-
 }
